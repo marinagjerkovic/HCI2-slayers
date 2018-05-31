1f0238e557c074dfc8392c2731126702e55b198e
ikijaem <ilija.malinovic@yahoo.com>
2018-05-31 03:21:01 +0200

    Revert "Revert "Pocetak je najtezi""
    This reverts commit 2955e7b53894c49826c704e074da802277f189cf.

---
+++ HCI2 - Back To Slay/HCI2 - Back To Slay/Classroom.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HCI2___Back_To_Slay
+{
+    class Classroom
+    {
+        public enum OpSystem
+        {
+            Windows,
+            Linux,
+            Both
+        };
+        private string id;
+        private string description;
+        private int num_of_seats;
+        private bool projector;
+        private bool board;
+        private bool smart_board;
+        private OpSystem os;
+        private List<Software> software;
+
+        public string Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        public string Description
+        {
+            get
+            {
+                return description;
+            }
+
+            set
+            {
+                description = value;
+            }
+        }
+
+        public int Num_of_seats
+        {
+            get
+            {
+                return num_of_seats;
+            }
+
+            set
+            {
+                num_of_seats = value;
+            }
+        }
+
+        public bool Projector
+        {
+            get
+            {
+                return projector;
+            }
+
+            set
+            {
+                projector = value;
+            }
+        }
+
+        public bool Board
+        {
+            get
+            {
+                return board;
+            }
+
+            set
+            {
+                board = value;
+            }
+        }
+
+        public bool Smart_board
+        {
+            get
+            {
+                return smart_board;
+            }
+
+            set
+            {
+                smart_board = value;
+            }
+        }
+
+        internal OpSystem Os
+        {
+            get
+            {
+                return os;
+            }
+
+            set
+            {
+                os = value;
+            }
+        }
+
+        internal List<Software> Software
+        {
+            get
+            {
+                return software;
+            }
+
+            set
+            {
+                software = value;
+            }
+        }
+
+        public Classroom() { }
+
+
+
+
+    }
+}
